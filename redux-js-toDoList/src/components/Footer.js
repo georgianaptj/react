d7b42e7197aa3b7dcd0bd8cfad4617ee127586a0
WHQ_NT_DOMAIN\GP083498 <georgi.ptj@gmail.com>
2020-08-24 18:49:20 +0300

    To do list demo using redux and JS

---
+++ redux-js-toDoList/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import FilterLink from "./FilterLink";
+import { VisibilityFilters } from "../redux/actions";
+
+//functional stateless components
+//where the user change currently visible todos
+const Footer = () => (
+  <div>
+    <span>Show: </span>
+    <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
+  </div>
+);
+
+export default Footer;
